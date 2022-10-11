e7e8c20827e17cecd7c3b0033693e14f2562f2ad
Kevin Frei <kevinfrei@hotmail.com>
2022-10-11 07:40:35 +0100

    Working on the circuit logic

--- src/Scoring.ts
+++ src/Scoring.ts
@@ -1,5 +1,7 @@
+// A simple 'score' type:
 export type Score = { red: number; blue: number };
 
+// A junction score: # of codes, plus whether it's owned or capped
 export type JScore = Score & {
   // Capital letter means 'beacon'
   owner?: 'r' | 'b' | 'R' | 'B';
@@ -17,8 +19,13 @@ export function getPos({ row, col }: RowCol): number {
   return col + row * 7;
 }
 
+function isCorner(i: RowCol) {
+  return (i.row === 0 || i.row === 6) && (i.col === 0 || i.col === 6);
+}
+
+// Calculate the value of each junction:
 export function junctionValueRC(row: number, col: number): number {
-  if ((col === 0 || col === 6) && (row === 0 || row === 6)) {
+  if (isCorner({ row, col })) {
     // For terminals, a negative # means check for side
     return -1;
   }
@@ -46,12 +53,15 @@ export function junctionValueRC(row: number, col: number): number {
   return 4;
 }
 
+// Calculate the value of junction (by flat number)
 export function junctionValue(pos: number): number {
   const { col, row } = getRC(pos);
   return junctionValueRC(row, col);
 }
 
+// This is used to score red/blue independently
 export type NeutralScore = { score: number; owned: boolean; capped: boolean };
+// Junction score to red score
 export function jToRed(junc: JScore): NeutralScore {
   return {
     score: junc.red,
@@ -59,6 +69,7 @@ export function jToRed(junc: JScore): NeutralScore {
     capped: junc.owner === 'R',
   };
 }
+// Junction score to blue score
 export function jToBlue(junc: JScore): NeutralScore {
   return {
     score: junc.blue,
@@ -67,6 +78,7 @@ export function jToBlue(junc: JScore): NeutralScore {
   };
 }
 
+// Given a function to call to get the 'neutral' score for a junction, get the side's score
 export function calcSide(
   getScore: (i: number) => NeutralScore,
   autoValue?: number,
@@ -93,4 +105,68 @@ export function calcSide(
     }
   }
   return total + (countFinal ? autoValue : 0);
+}
+
+function redCircuit(getScore: (i: number) => NeutralScore): boolean {
+  return connected(getScore, { row: 0, col: 6 }, { row: 6, col: 0 });
+}
+
+function blueCircuit(getScore: (i: number) => NeutralScore): boolean {
+  return connected(getScore, { row: 6, col: 6 }, { row: 0, col: 0 });
+}
+
+// INTERVIEW QUESTION INCOMING!!!!
+function connected(
+  getScore: (i: number) => NeutralScore,
+  from: RowCol,
+  to: RowCol,
+): boolean {
+  if (getScore(getPos(from)).score === 0 || getScore(getPos(to)).score === 0) {
+    return false;
+  }
+  function emptyVisitor(): string {
+    return '1234567abcdefg1234567abcdefg1234567abcdefg1234567';
+  }
+  function isVisited(flags: string, rc: RowCol): boolean {
+    return flags.charAt(getPos(rc)) === 'X';
+  }
+  function setVisited(flags: string, rc: RowCol): string {
+    const pos = getPos(rc);
+    return flags.substring(0, pos) + 'X' + flags.substring(pos + 1);
+  }
+  function clrVisited(flags: string, rc: RowCol): string {
+    const pos = getPos(rc);
+    return flags.substring(0, pos) + '-' + flags.substring(pos + 1);
+  }
+  const start = setVisited(emptyVisitor(), from);
+  type WorkItem = { visited: string; rc: RowCol };
+  // Seed the worklist with the initial board & starting position:
+  const worklist: WorkItem[] = [{ visited: start, rc: from }];
+  while (worklist.length > 0) {
+    // Pull the back item from the worklist:
+    const item = worklist.pop();
+    if (item === undefined) {
+      window.alert('ERROR!');
+      return false;
+    }
+    // Check to see if this location connect with "to"
+    if (connects(item.rc, to)) {
+      return true;
+    }
+    // TODO: Continue here
+  }
+  return true;
+}
+
+// This is a helpful place to deal with corners...
+function connects(a: RowCol, b: RowCol): boolean {
+  if (Math.abs(a.row - b.row) < 2 && Math.abs(a.col - b.col) < 2) {
+    return true;
+  }
+  if (isCorner(a) || isCorner(b)) {
+    const xd = a.row - b.row;
+    const yd = a.col - b.col;
+    return xd * xd + yd * yd < 6;
+  }
+  return false;
 }
